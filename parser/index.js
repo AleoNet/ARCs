cfff8f86a1ab945d918982008e9a5921f8b8e8eb
howardwu <howardwu@berkeley.edu>
2021-02-07 13:22:08 -0800

    Add additional sanity checks

--- parser/index.js
+++ parser/index.js
@@ -2,6 +2,21 @@ const fs = require('fs');
 const glob = require("glob");
 const parseMD = require('parse-md').default;
 
+/**
+ * arc: 1
+ * title: Template
+ * authors: The Aleo Team <hello@aleo.org>
+ * topic: Meta
+ * status: Living
+ * reviewers: Howard Wu <howard@aleo.org>
+ * created: 2020-02-07
+ */
+const requiredMetadata = ['arc', 'title', 'authors', 'topic', 'status', 'reviewers', 'created'];
+
+const topics = ['Meta', 'Protocol', 'Network', 'Application'];
+
+const statuses = ['Idea', 'Draft', 'Active', 'Withdrawn', 'Accepted', 'Rejected', 'Final', 'Deprecated', 'Living'];
+
 // Executes sanity checks for CI.
 const ci = () => {
     getArcDirectories('..', async (err, list) => {
@@ -120,7 +135,6 @@ const checkArcReadmeContent = async (arcDirectory) => {
      * reviewers: Howard Wu <howard@aleo.org>
      * created: 2020-02-07
      */
-    const requiredMetadata = ['arc', 'title', 'authors', 'topic', 'status', 'reviewers', 'created'];
 
     // Check that the metadata is fully filled in.
     for (let i = 0; i < requiredMetadata.length; i++) {
@@ -129,19 +143,64 @@ const checkArcReadmeContent = async (arcDirectory) => {
             console.error('\n', arcReadmeFile, 'is missing \'', metatopic, '\'.\n');
             process.exit(1);
         }
-    }
 
-    // Check that the ARC # matches the directory #.
-    try {
-        const directoryNumber = parseInt(arcDirectory.split('-')[1]);
-        const metadataNumber = parseInt(metadata.arc);
-        if (directoryNumber !== metadataNumber) {
-            console.error('\nARC directory ID (', arcDirectory, ') does not match the ARC ID in the README (', metadataNumber, ').\n');
-            process.exit(1);
+        // Check that the ARC # matches the directory #.
+        if (metatopic === 'arc') {
+            try {
+                const directoryNumber = parseInt(arcDirectory.split('-')[1]);
+                const metadataNumber = parseInt(metadata.arc);
+                if (directoryNumber !== metadataNumber) {
+                    console.error('\nARC directory ID (', arcDirectory, ') does not match the ARC ID in the README (', metadataNumber, ').\n');
+                    process.exit(1);
+                }
+            } catch (err) {
+                console.error("\nFailed to check that", arcDirectory, "has a matching ARC number.\n");
+                process.exit(1)
+            }
         }
-    } catch (err) {
-        console.error("\nFailed to check that", arcDirectory, "has a matching ARC number.\n");
-        process.exit(1)
+
+        // Check that the ARC title is nonempty.
+        if (metatopic === 'title') {
+            const title = metadata['title'];
+            if (title === null || title === undefined || title === "") {
+                console.error('\nARC title cannot be empty.\n');
+                process.exit(1);
+            }
+        }
+
+        // Check that the ARC authors is nonempty.
+        if (metatopic === 'authors') {
+            const authors = metadata['authors'];
+            if (authors === null || authors === undefined || authors === "") {
+                console.error('\nARC authors cannot be empty.\n');
+                process.exit(1);
+            }
+        }
+
+        // Check that the ARC topic matches a valid topic.
+        if (metatopic === 'topic') {
+            if (!topics.includes(metadata['topic'])) {
+                console.error('\nARC topic (', metadata['topic'], ') is not an accepted topic.\n');
+                process.exit(1);
+            }
+        }
+
+        // Check that the ARC status matches a valid status.
+        if (metatopic === 'status') {
+            if (!statuses.includes(metadata['status'])) {
+                console.error('\nARC status (', metadata['status'], ') is not an accepted status.\n');
+                process.exit(1);
+            }
+        }
+
+        // Check that the ARC created is nonempty.
+        if (metatopic === 'created') {
+            const created = metadata['created'];
+            if (created === null || created === undefined || created === "") {
+                console.error('\nARC created cannot be empty.\n');
+                process.exit(1);
+            }
+        }
     }
 
     // Check that the main body is not empty.
